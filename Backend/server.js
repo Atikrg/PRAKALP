736147020814a6fb99c9e05a2a2a2d07b1ac0965
Atik Salim Rangnekar <52129545+Atikrg@users.noreply.github.com>
2024-08-20 15:04:11 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const ipAddr = '127.0.0.1';
 
 app.listen(process.env.PORT, ipAddr, () => {
-  console.log(`Server is running on http://${ipAddr}:${process.env.SERVER_PORT}`);
+  console.log(`Server is running on http://${ipAddr}:${process.env.PORT}`);
 });
